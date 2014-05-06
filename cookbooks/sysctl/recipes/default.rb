2d73fe74a6aa3d96a6029b359612f294350ca756
wataken44 <wataken44@macaron.in.w5n.org>
2014-05-07 08:23:40 +0900

    [A] add sysctl cookbook

---
+++ cookbooks/sysctl/recipes/default.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: sysctl
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+template "/etc/sysctl.conf" do
+    source "sysctl.conf.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables(:variables => node["sysctl"]["variables"])
+    action :create
+end
+
+execute "reload sysctl.conf" do
+    command "echo 'reload sysctl.conf(sudo /sbin/sysctl -p)'; sudo /sbin/sysctl -p"
+    action :run
+end
+

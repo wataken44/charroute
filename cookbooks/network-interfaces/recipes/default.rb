24381f6e3af5153c7242e748e88c9922a9da4cf8
wataken44 <wataken44@macaron.in.w5n.org>
2014-05-06 16:49:31 +0900

    [A] add network-interfaces recipe

---
+++ cookbooks/network-interfaces/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: network-interfaces
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+template "/etc/network/interfaces" do
+    source "network-interfaces.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables(:interfaces => node["network-interfaces"]["interfaces"])
+    action :create
+end
+
+service "networking" do
+    action :restart
+end

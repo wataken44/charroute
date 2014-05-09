a32b23930b13f5bcea795dc6ddae63961e7ac526
wataken44 <wataken44@icecream>
2014-05-10 01:11:22 +0900

    [F] fix that cookbook_name cannot be used in attributes:(

--- cookbooks/ntp/attributes/default.rb
+++ cookbooks/ntp/attributes/default.rb
@@ -1,5 +1,7 @@
 # ntp/attributes/default.rb
 
+cookbook_name = 'ntp'
+
 default[cookbook_name]['panic'] = nil
 default[cookbook_name]['broadcast'] = nil
 default[cookbook_name]['servers'] = ["ntp.jst.mfeed.ad.jp"]

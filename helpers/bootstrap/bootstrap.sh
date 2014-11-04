08af736a3bb13cc62f4104e45be831da174ae6df
wataken44 <wataken44@sundae.in.w5n.org>
2014-11-04 23:14:02 +0900

    [F] fix missing package build-essential

--- helpers/bootstrap/bootstrap.sh
+++ helpers/bootstrap/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apt-get update
-apt-get install ruby ruby-dev make sudo git-core -y
+apt-get install ruby ruby-dev make sudo build-essential git-core -y
 
 gem update
 gem install chef --no-rdoc --no-ri
